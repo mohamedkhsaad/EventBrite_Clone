c3c26ee91830eaa420b907a84ba70cdbce45918f
DanielRafik <73848754+DanielRafik@users.noreply.github.com>
2023-03-21 04:26:54 +0200

    e

--- eventbrite/user/urls.py
+++ eventbrite/user/urls.py
@@ -1,7 +1,15 @@
 from rest_framework import routers
 from .views import*
+from django.urls import path,include
 
 router=routers.SimpleRouter()
 router.register('',userViewSet)
 # router.register('Interests',user_interests_ViewSet)
 urlpatterns =router.urls
+
+urlpatterns = [
+    path('signup/', userViewSet.as_view({'post': 'create'}), name='signup'),
+    # path('login/', LoginView.as_view(), name='login'),
+    
+
+]
--- eventbrite/user/views.py
+++ eventbrite/user/views.py
@@ -2,64 +2,54 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
 from django.shortcuts import render
 from rest_framework import serializers
 from rest_framework import viewsets,status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-
-# from .models import*
+from rest_framework.views import APIView
+from .models import*
 # Create your views here.
 
 '''
 user model 
 '''
 class userSerializer(serializers.ModelSerializer):
-    
-    # first_name=serializers.CharField(max_length=20)
-    # last_name=serializers.CharField(max_length=20)
-    # email=serializers.EmailField()
-    # password=serializers.CharField(max_length=20)
+    password = serializers.CharField(write_only=True)
 
-    # username =serializers.CharField(max_length=20)
-    # age =serializers.IntegerField()
-    # # BIRTH_DATE=serializers.DateField()
-    # # PHONE =serializers.CharField(max_length=20)
-
-    # CITY=serializers.CharField(max_length=20)
-    # COUNTRY=serializers.CharField(max_length=20)
-
-    # ADDRESS =serializers.CharField(max_length=20)
-    # LOCATION_ID =serializers.IntegerField()
-    # DISCOUNT_ID=serializers.IntegerField()
-    # INTERESTS_ID=serializers.ListField()
-    # EVENT_CREATED=serializers.ListField()
-    # TICKETS_ID=serializers.ListField()
-    # FOLLOWERS=serializers.ListField()
     class Meta:
-        model=User
-        fields = ['email','first_name','last_name','password','username'] 
-    def create_user(self,validated_data):
-        user=User.objects.create_user(
-            email=validated_data['email'],
-            F_NAME=validated_data['first_name'],
-            L_NAME=validated_data['last_name'],
-            username=validated_data['username'],
-            password=validated_data['password']
-           
-            # lname=validated_data['lname'],
-        )
-        user.set_password(validated_data['password'])
+        model = User
+        fields = ['email', 'first_name', 'last_name', 'password', 'age', 'gender', 'city', 'country']
+
+    def create(self, validated_data):
+        email=validated_data.pop('email')
+        password = validated_data.pop('password')
+        user = User.objects.create_user(**validated_data, password=password,username=email)
         user.save()
         return user
-    
+
+# class LoginView(APIView):
+#     def post(self, request, format=None):
+#         data = request.data
+#         email = data.get('email', None)
+#         password = data.get('PASSWORD', None)
+#         user = authenticate(username=email, password=password)
+#         if user is not None:
+#             login(request, user)
+#             serializer = userSerializer(user)
+#             return Response(serializer.data)
+#         else:
+#             return Response({'error': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
 
 @api_view(['POST'])
 def create_user(request):
     serializer=userSerializer(data=request.data)
     if serializer.is_valid():
         serializer.save()
-        return Response(serializer,status=status.HTTP_201_CREATED)
+        return Response(serializer.data,status=status.HTTP_201_CREATED)
+    else:
+        Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         
 
 class userViewSet(viewsets.ModelViewSet):
@@ -69,9 +59,9 @@ class userViewSet(viewsets.ModelViewSet):
     serializer_class = userSerializer
     queryset = User.objects.all()
 
-# '''               
-# user interests model 
-# '''
+'''
+user interests model 
+'''
 
 # class user_interests_Serializer(serializers.ModelSerializer):
 #     class Meta:
