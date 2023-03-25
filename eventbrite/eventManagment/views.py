870a2ab4a2f7c07e2652f0608950d37a79eef454
Abdelrhman-Elshahed <73655857+Abdelrhman-Elshahed@users.noreply.github.com>
2023-03-25 05:10:06 +0200

    Update views.py

--- eventbrite/eventManagment/views.py
+++ eventbrite/eventManagment/views.py
@@ -1,42 +1,64 @@
-from django.shortcuts import render
+"""
+This module contains several view classes for the eventmanagement app.
+class:UserListEvents: A viewset for retrieving all user events by user id.
+class:UserListPastEvents: A viewset for retrieving all user past events by user id.
+class:UserListEvents: A viewset for retrieving all user upcoming events by user id.
+"""
 from django.shortcuts import render
 from rest_framework import serializers
 from rest_framework import viewsets
 from django.http import JsonResponse, HttpResponse
-from event.serializers import*
+from .serializers import*
 from rest_framework import generics
 from user import*
-from rest_framework.response import Response
-from rest_framework.views import APIView
-from rest_framework.permissions import IsAuthenticated
 from datetime import date
-from event.models import event
-
-
+import csv
+from .models import event
 
 class UserListEvents(generics.ListAPIView):
-    permission_classes = [IsAuthenticated]
+    """
+    A viewset for retrieving all user events by user id.
+    """
     queryset = event.objects.all()
     serializer_class = eventSerializer
+
     def get_queryset(self):
+        """
+        This view should list all the user events
+        for the given user id.
+        """
         user_id = self.kwargs['user_id']
         return event.objects.filter(User_id=user_id)
 
 class UserListPastEvents(generics.ListAPIView):
-    permission_classes = [IsAuthenticated]
+    """
+    A viewset for retrieving all user past events by user id.
+    """
     today = date.today()
     queryset = event.objects.all()
     serializer_class = eventSerializer
+
     def get_queryset(self):
+        """
+        This view should list all the user past 
+        events for the given user id.
+        """
         user_id = self.kwargs['user_id']
         return event.objects.filter(User_id=user_id).filter(ST_DATE__lt=self.today)
 
 class UserListUpcomingEvents(generics.ListAPIView):
-    permission_classes = [IsAuthenticated]
+    """
+    A viewset for retrieving all user upcoming events by user id.
+    """
     today = date.today()
     queryset = event.objects.all()
     serializer_class = eventSerializer
+
     def get_queryset(self):
+        """
+        This view should list all the user upcoming
+        events for the given user id.
+        """
         user_id = self.kwargs['user_id']
         return event.objects.filter(User_id=user_id).filter(ST_DATE__gt=self.today)
     
