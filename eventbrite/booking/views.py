c13a287b3c03b609a816a2b5eecb3528f28ed489
RamiDiaa <ramidiaa0@gmail.com>
2023-03-25 15:30:37 +0200

    added auth. for booking app
    added auth. for booking app check views of the app and the unit test

--- eventbrite/booking/views.py
+++ eventbrite/booking/views.py
@@ -1,45 +1,116 @@
 from django.shortcuts import render
 from django.core.mail import send_mail
+from django.http import Http404
 
 from eventbrite.email_info import from_email
 
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
 from rest_framework.response import Response
 from rest_framework import generics, filters, status
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 from .serializers import TicketSerializer, DiscountSerializer
 from event.serializers import eventSerializer
-# models
+
 from .models import Ticket,Discount
 from event.models import event as Event
 from user.models import User
 
-from django.http import Http404
 
 
 
-# TEMP: ticket generics view sets mainly for adding objects into the database and testing
-class TicketList(generics.ListCreateAPIView):
-    queryset = Ticket.objects.all()
-    serializer_class = TicketSerializer
-    # authentication_classes = [TokenAuthentication]
-    # permission_classes = [IsAuthenticated]
+
+
+
+
+
+# DONE 
+# DONE TESTING
+@api_view(['GET'])
+@authentication_classes([TokenAuthentication])
+@permission_classes([IsAuthenticated])
+def list_tickets_by_event(request, event_id):
+
+    # get all tickets for this event
+    tickets = Ticket.objects.filter(EVENT_ID=event_id)
+    serialized_tickets = TicketSerializer(tickets, many=True)
+
+    # return the data as a  list of JSON objects
+    return Response(serialized_tickets.data)
+
+
+# DONE TESTING
+@api_view(['GET'])
+@authentication_classes([TokenAuthentication])
+@permission_classes([IsAuthenticated])
+def list_tickets_by_user(request, user_id):
+    # get all tickets for this event
+    tickets = Ticket.objects.filter(GUEST_ID=user_id)
+    serialized_tickets = TicketSerializer(tickets, many=True)
+
+    # return the data as a  list of JSON objects
+    return Response(serialized_tickets.data)
+
+
+
+# DONE
+# DONE TESTING
+@api_view(['GET'])
+@authentication_classes([TokenAuthentication])
+@permission_classes([IsAuthenticated])
+def get_ticket(request,ticket_id):
+    # /ticket/{ticket_id}
+
+    try:
+        print(ticket_id)
+        ticket = Ticket.objects.get(id=ticket_id)
+    except Ticket.DoesNotExist:
+        raise Http404
+
+    serialized_ticket = TicketSerializer(ticket, many=False)
+    return Response(serialized_ticket.data,status=status.HTTP_200_OK)
+
+
+# check
+# DONE TESTING
+@api_view(['GET'])
+@authentication_classes([TokenAuthentication])
+@permission_classes([IsAuthenticated])
+def check_promo_code(request,event_id):
+    # /event?promo_code=SAVE123
+    # search an event's promo codes
+    try:
+        promo_code = request.query_params['promo_code']
+    except:
+        return Response({'err':'missing promo_code param'},status=status.HTTP_400_BAD_REQUEST)
+    
+    if not Discount.objects.get(EVENT_ID=event_id,CODE=promo_code):
+        return Response({'is_promo_code':False},status=status.HTTP_400_BAD_REQUEST)
+
+    # return ticket object instead
+    return Response({'is_promo_code':True},status=status.HTTP_200_OK)
+
 
 
 
 class discount_list(generics.ListCreateAPIView):
     queryset = Discount.objects.all()
     serializer_class = DiscountSerializer
-    # authentication_classes = [TokenAuthentication]
-    # permission_classes = [IsAuthenticated]
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
 class discount_pk(generics.RetrieveUpdateDestroyAPIView):
     queryset = Discount.objects.all()
     serializer_class = DiscountSerializer
-
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
 
 # in-progress
 #TODO: configer email service backend
 @api_view(['POST'])
+# @authentication_classes([TokenAuthentication])
+# @permission_classes([IsAuthenticated])
 def create_ticket(request):
  
     # TODO:
@@ -73,60 +144,10 @@ def create_ticket(request):
         return Response(ticket_serializer.data, status=201)
     return Response(ticket_serializer.errors, status=400)
 
-# DONE 
-# DONE TESTING
-@api_view(['GET'])
-def list_tickets_by_event(request, event_id):
 
-    # get all tickets for this event
-    tickets = Ticket.objects.filter(EVENT_ID=event_id)
-    serialized_tickets = TicketSerializer(tickets, many=True)
-
-    # return the data as a  list of JSON objects
-    return Response(serialized_tickets.data)
-
-
-# DONE TESTING
-@api_view(['GET'])
-def list_tickets_by_user(request, user_id):
-    # get all tickets for this event
-    tickets = Ticket.objects.filter(GUEST_ID=user_id)
-    serialized_tickets = TicketSerializer(tickets, many=True)
-
-    # return the data as a  list of JSON objects
-    return Response(serialized_tickets.data)
-
-
-
-# DONE
-# DONE TESTING
-@api_view(['GET'])
-def get_ticket(request,ticket_id):
-    # /ticket/{ticket_id}
-
-    try:
-        print(ticket_id)
-        ticket = Ticket.objects.get(id=ticket_id)
-    except Ticket.DoesNotExist:
-        raise Http404
-
-    serialized_ticket = TicketSerializer(ticket, many=False)
-    return Response(serialized_ticket.data,status=status.HTTP_200_OK)
-
-
-# check
-# DONE TESTING
-@api_view(['GET'])
-def check_promo_code(request,event_id):
-    # /event?promo_code=SAVE123
-    # search an event's promo codes
-    try:
-        promo_code = request.query_params['promo_code']
-    except:
-        return Response({'err':'missing promo_code param'},status=status.HTTP_400_BAD_REQUEST)
-    
-    if not Discount.objects.get(EVENT_ID=event_id,CODE=promo_code):
-        return Response({'is_promo_code':False},status=status.HTTP_400_BAD_REQUEST)
-
-    # return ticket object instead
-    return Response({'is_promo_code':True},status=status.HTTP_200_OK)
+# TEMP: ticket generics view sets mainly for adding objects into the database and testing
+class TicketList(generics.ListCreateAPIView):
+    queryset = Ticket.objects.all()
+    serializer_class = TicketSerializer
+    # authentication_classes = [TokenAuthentication]
+    # permission_classes = [IsAuthenticated]
