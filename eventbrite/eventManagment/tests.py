d5b8be12abc5b45ec9a31e4415a1cbc5b98e97eb
Ismail Tawfik <elshelyziad2gmail.com>
2023-03-25 07:08:26 +0200

    documntation

--- eventbrite/eventManagment/tests.py
+++ eventbrite/eventManagment/tests.py
@@ -1,26 +1,22 @@
 from django.test import TestCase
 from rest_framework.test import APIClient, APITestCase
-from .models import event
+from event.models import event
 from datetime import date, time
 from rest_framework import status
-from .serializers import *
+from event.serializers import *
 
 
 class EventManagementTest(APITestCase):
-    
-    
+
     def setUp(self):
-        
-        eventus = event.objects.create(ID= 1, User_id=1190507, Title="mmatalk", organizer='Abdelrhman Elshahed', Description="mmatalk", type="chatting", Category='sport', sub_Category='mma', venue_name='giza', CATEGORY_ID=1, SUB_CATEGORY_ID=1, ST_DATE=date(2023, 3, 5), END_DATE=date(2023, 4, 1), ST_TIME=time(15, 00, 00), END_TIME=time(16, 00, 00), ONLINE=True, CAPACITY=500, PASSWORD=123456, location_id=1, STATUS='open')
 
-
-
-
+        eventus = event.objects.create(ID=1, User_id=1190507, Title="mmatalk", organizer='Abdelrhman Elshahed', Description="mmatalk", type="chatting", Category='sport', sub_Category='mma', venue_name='giza', CATEGORY_ID=1,
+                                       SUB_CATEGORY_ID=1, ST_DATE=date(2023, 3, 5), END_DATE=date(2023, 4, 1), ST_TIME=time(15, 00, 00), END_TIME=time(16, 00, 00), ONLINE=True, CAPACITY=500, PASSWORD=123456, location_id=1, STATUS='open')
 
     def test_retrieve_all_user_events_by_user_id(self):
         self.client = APIClient()
         user_id = 1190507
-        eventus = event.objects.filter(User_id= user_id)
+        eventus = event.objects.filter(User_id=user_id)
         serializer = eventSerializer(eventus, many=True)
         url = f"http://127.0.0.1:8000/events/list_user_events/{user_id}/"
         response = self.client.get(url, format='json')
@@ -31,7 +27,8 @@ class EventManagementTest(APITestCase):
         self.client = APIClient()
         user_id = 1190507
         today = date.today()
-        eventus = event.objects.filter(User_id=user_id).filter(ST_DATE__lt=today)
+        eventus = event.objects.filter(
+            User_id=user_id).filter(ST_DATE__lt=today)
         serializer = eventSerializer(eventus, many=True)
         url = {f'http://127.0.0.1:8000/events/list_user_past_events/{user_id}/'}
         response = self.client.get(url, format='json')
@@ -42,7 +39,8 @@ class EventManagementTest(APITestCase):
         self.client = APIClient()
         user_id = 1190507
         today = date.today()
-        eventus = event.objects.filter(User_id=user_id).filter(ST_DATE__gt=today)
+        eventus = event.objects.filter(
+            User_id=user_id).filter(ST_DATE__gt=today)
         serializer = eventSerializer(eventus, many=True)
         url = {f'http://127.0.0.1:8000/events/list_user_past_events/{user_id}/'}
         response = self.client.get(url, format='json')
