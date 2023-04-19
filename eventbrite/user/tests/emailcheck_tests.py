422db20df3ef764ab5409c3b7b6154ab02538fb6
DanielRafik <73848754+DanielRafik@users.noreply.github.com>
2023-04-19 03:08:28 +0200

    unit tests update done

--- eventbrite/user/tests/emailcheck_tests.py
+++ eventbrite/user/tests/emailcheck_tests.py
@@ -2,13 +2,13 @@ from django.test import TestCase
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APIClient
-from django.contrib.auth import get_user_model
+from ..models import User
 
 
 class EmailCheckViewTestCase(TestCase):
     def setUp(self):
         self.client = APIClient()
-        self.user_model = get_user_model()
+        self.user_model = User
         self.user_data = {
             'email': 'testuser@example.com',
             'password': 'testpassword',
@@ -16,16 +16,16 @@ class EmailCheckViewTestCase(TestCase):
         }
         
         self.user = self.user_model.objects.create_user(**self.user_data)
-        self.url = reverse('email-check', kwargs={'email': self.user_data['email']}) + '/'
+        self.url = reverse('email-check', args=[self.user_data['email']])
 
     def test_email_check_returns_true_for_existing_user(self):
-        response = self.client.get(self.url)
+        response = self.client.get(self.url, follow=True)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertJSONEqual(response.content, {'email_exists': True})
 
     def test_email_check_returns_false_for_non_existing_user(self):
         non_existing_email = 'nonexistinguser@example.com'
-        url = reverse('email-check', kwargs={'email': non_existing_email})
-        response = self.client.get(url)
+        url = reverse('email-check', args=[non_existing_email])
+        response = self.client.get(url, follow=True)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertJSONEqual(response.content, {'email_exists': False})
