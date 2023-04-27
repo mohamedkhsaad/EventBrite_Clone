4019e2ee6524aad81aad9731874438689c2f64a3
Ismail Tawfik <elshelyziad2gmail.com>
2023-04-27 14:25:15 +0200

    edit migrations

--- eventbrite/event/migrations/0046_event_user.py
+++ eventbrite/event/migrations/0046_event_user.py
@@ -16,7 +16,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='event',
             name='user',
-            field=models.ForeignKey(default='ww', on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL),
+            field=models.ForeignKey(default=1, on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL),
             preserve_default=False,
         ),
     ]
